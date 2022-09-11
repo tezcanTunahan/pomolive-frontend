9b9aa793198990495e069c6e0fe70b48f5177c90
Tunahan Tezcan <ttezcan.1999@gmail.com>
2022-09-11 23:21:02 +0300

    delete unnececery button

--- components/Timer.jsx
+++ components/Timer.jsx
@@ -1,20 +1,17 @@
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import PlayButton from './PlayButton';
-import PauseButton from './PauseButton';
-import SettingButtons from './SettingButtons';
+import Button from './UI/Button';
 import { useContext, useState, useEffect, useRef } from 'react';
 import SettingsContext from '../store/SettingsContext';
-import SwitchButton from '../components/SwitchButton';
 
 export default function Timer() {
   const settingsInfo = useContext(SettingsContext);
 
-  const [isPaused, setIsPaused] = useState(false);
+  const [isPaused, setIsPaused] = useState(true);
   const [mode, setMode] = useState('work');
   const [secondsLeft, setSecondsLeft] = useState(0);
 
-  const isPausedRef = useRef(false);
+  const isPausedRef = useRef(isPaused);
   const modeRef = useRef(mode);
   const secondsLeftRef = useRef(secondsLeft);
 
@@ -32,6 +29,9 @@ export default function Timer() {
     modeRef.current = nextMode;
     setSecondsLeft(nextSeconds);
     secondsLeftRef.current = nextSeconds;
+
+    isPausedRef.current = true;
+    setIsPaused(isPausedRef);
   };
   const tick = () => {
     secondsLeftRef.current = secondsLeftRef.current - 1;
@@ -57,7 +57,7 @@ export default function Timer() {
           text={`${minute}: ${seconds}`}
           styles={buildStyles({
             // Colors
-            pathColor: mode === 'work' ? 'red' : 'green',
+            pathColor: mode === 'work' ? 'red' : '#08fdd8',
             textColor: '#f88',
             trailColor: 'rgb(255, 255, 255.2)',
             backgroundColor: '#3e98c7',
@@ -66,22 +66,87 @@ export default function Timer() {
       </div>
       <div className='timer__button'>
         {isPaused ? (
-          <PlayButton
+          <Button
             onClick={() => {
               setIsPaused(false);
               isPausedRef.current = false;
             }}
+            text={
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='w-6 h-6'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z'
+                />
+              </svg>
+            }
           />
         ) : (
-          <PauseButton
+          <Button
             onClick={() => {
               setIsPaused(true);
               isPausedRef.current = true;
             }}
+            text={
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='w-6 h-6'
+              >
+                <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 5.25v13.5m-7.5-13.5v13.5' />
+              </svg>
+            }
           />
         )}
-        <SettingButtons onClick={() => settingsInfo.setShowSetting(true)} />
-        <SwitchButton onClick={switchHandle} />
+        <Button
+          onClick={() => settingsInfo.setShowSetting(true)}
+          text={
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='w-6 h-6'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z'
+              />
+              <path strokeLinecap='round' strokeLinejoin='round' d='M15 12a3 3 0 11-6 0 3 3 0 016 0z' />
+            </svg>
+          }
+        />
+        <Button
+          onClick={switchHandle}
+          text={
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='w-6 h-6'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M7.5 21L3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5'
+              />
+            </svg>
+          }
+        />
       </div>
     </div>
   );
